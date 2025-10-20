14105eb0c39e97351bd27e5ee72a972091215d68
torah <torah1803@gmail.com>
2025-10-20 22:58:22 +0700

    CRUD

--- src/person/person.entity.ts
+++ src/person/person.entity.ts
@@ -1,38 +1,46 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn, JoinColumn } from 'typeorm';
 import { Household } from '../household/household.entity';
 
-@Entity()
+@Entity('persons')
 export class Person {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn({name : 'id'})
   id: number;
 
-  @Column()
+  @Column({name : 'full_name'})
   fullName: string;
 
-  @Column()
+  @Column({name : 'date_of_birth'})
   dateOfBirth: Date;
 
-  @Column()
+  @Column({name : 'gender'})
   gender: string;
 
-  @Column()
+  @Column({name : 'identification_number'})
   identificationNumber: string; // CMND/CCCD
 
-  @Column({ nullable: true })
-  relationshipWithHouseholdHead: string;
+  @Column({ nullable: true, name: 'relationship_with_head' })
+  relationshipWithHead: string;
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, name: 'occupation' })
   occupation: string;
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, name: 'education_level' })
   educationLevel: string;
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, name: 'migration_status' })
   migrationStatus: string; // "Thường trú", "Tạm trú", v.v.
 
-  @Column({ nullable: true })
+  @Column({ nullable: true, name: 'is_deceased' })
   isDeceased: boolean;
 
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+  createdAt: Date;
+
+  
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+  updatedAt: Date;
+
   @ManyToOne(() => Household, (household) => household.members)
+  @JoinColumn({name : 'household_id'})
   household: Household;
 }
