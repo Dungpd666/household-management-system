14c00f167b754c73eaddecedba665a1ffc551fd8
thetree303 <kiennguyen300305@gmail.com>
2025-12-04 22:10:17 +0700

    fix: household entity

--- src/household/household.entity.ts
+++ src/household/household.entity.ts
@@ -8,6 +8,7 @@ import {
 } from 'typeorm';
 
 import { Person } from '../person/person.entity';
+import { Contribution } from 'src/contribution/contribution.entity';
 
 @Entity('households')
 export class Household {
@@ -40,47 +41,7 @@ export class Household {
 
   @OneToMany(() => Person, (person) => person.household)
   members: Person[];
-}
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  OneToMany,
-  CreateDateColumn,
-  UpdateDateColumn,
-} from 'typeorm';
 
-import { Person } from '../person/person.entity';
-
-@Entity('households')
-export class Household {
-  @PrimaryGeneratedColumn({ name: 'id' })
-  id: number;
-
-  @Column({ name: 'household_code' })
-  householdCode: string; // Mã hộ khẩu
-
-  @Column({ name: 'address' })
-  address: string;
-
-  @Column({ name: 'ward' })
-  ward: string; // Phường/xã
-
-  @Column({ name: 'district' })
-  district: string;
-
-  @Column({ name: 'city' })
-  city: string;
-
-  @Column({ name: 'household_type' })
-  householdType: string;
-
-  @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
-  createdAt: Date;
-
-  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
-  updatedAt: Date;
-
-  @OneToMany(() => Person, (person) => person.household)
-  members: Person[];
+  @OneToMany(() => Contribution, (contribution) => contribution.household)
+  contributions: Contribution[];
 }
