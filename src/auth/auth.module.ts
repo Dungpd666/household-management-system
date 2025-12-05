1773b7fcca0ad55acc0b18c05499592a4cc2cf18
Trịnh Phú Thái <140522495+thaiphu05@users.noreply.github.com>
2025-12-05 13:27:42 +0700

    feat: add passport-jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,10 +6,11 @@ import { UsersModule } from 'src/users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy'
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   imports: [
     UsersModule,
     ConfigModule,
