b4ad28fc4dfacdf7a045fb9a5d8f2a150478a5a5
Trịnh Phú Thái <140522495+thaiphu05@users.noreply.github.com>
2025-12-05 13:28:53 +0700

    fix: group statistic req, add jwtstrategy in users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Param, Post, Body, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, Put, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { PersonService } from '../person/person.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PassportJwtGuard } from '../auth/guard/passport-jwt.guard'
 
+@UseGuards(PassportJwtGuard)
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService, 
@@ -17,6 +19,18 @@ export class UsersController {
     @Post()
     async CreateUser(@Body() dto: CreateUserDto){
         return this.usersService.createUser(dto, dto.role);
+    }
+    
+    @Get('population')
+    async Statistic(){
+        const age = await this.personService.ageGroup();
+        const job = await this.personService.jobGroup();
+        const gender = await this.personService.jobGroup();
+        return {
+            "Age": age,
+            "Job": job,
+            "Gender": gender
+        };
     }
 
     @Get(':id')
@@ -33,20 +47,4 @@ export class UsersController {
     async RemoveUser(@Param('id') id : number){
         return this.usersService.removeUser(id);
     }
-
-    @Get('/population/age')
-    async AgeGroup(){
-        return this.personService.ageGroup();
-    }
-
-    @Get('/population/job')
-    async JobGroup(){
-        return this.personService.jobGroup();
-    }
-
-    @Get('/population/gender')
-    async GenderGroup(){
-        return this.personService.genderGroup();
-    }
-
 }
