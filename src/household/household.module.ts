7dfafadc568351736e4d31ea1ddc3f05e54c1f3d
dungpd666 <pducdung.it@gmail.com>
2025-12-19 16:45:29 +0700

    fix: front end config problems

--- src/household/household.module.ts
+++ src/household/household.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 import { Household } from './household.entity';
 import { Person } from '../person/person.entity';
 import { HouseholdService } from './household.service';
 import { HouseholdController } from './household.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Household, Person])],
+  imports: [
+    TypeOrmModule.forFeature([Household, Person]),
+    JwtModule,
+    ConfigModule,
+  ],
   controllers: [HouseholdController],
   providers: [HouseholdService],
   exports: [HouseholdService],
